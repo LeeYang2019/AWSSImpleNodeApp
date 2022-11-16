26c06fe1989f9c341df45cabd2ed5aaa4cd87f2b
leeyang2019 <nyang@madisoncollege.edu>
2022-11-15 23:38:30 -0600

    made changes

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('service is up and running for Lee Yang'));
 
+app.get('/who', (req, res) =>
+	res.send('Lee Yang is an amazing guy. He is working on improving himself.')
+);
+
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
